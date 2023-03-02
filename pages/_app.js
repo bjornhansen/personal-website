cb0ef782d2701403b99b57787c6c25103f3dcda0
Bjorn Hansen <blmhansen@gmail.com>
2023-03-01 21:43:09 -0800

    Add Google Analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,5 @@
 import {Inter, Open_Sans} from 'next/font/google'
+import Script from 'next/script';
 
 const inter = Inter({subsets: ['latin'], variable: '--font-inter'})
 const openSans = Open_Sans({subsets: ['latin'], variable: '--font-open-sans'})
@@ -8,6 +9,17 @@ import '@/styles/globals.css'
 export default function App({Component, pageProps}) {
     return (
         <main className={`${openSans.variable} font-sans ${inter.variable}`}>
+            <Script
+                src="https://www.googletagmanager.com/gtag/js?id=G-5XQNVP34CP"
+                strategy="afterInteractive"
+            ></Script>
+            <Script id="google-analytics" strategy="afterInteractive">
+                {`window.dataLayer = window.dataLayer || [];
+                function gtag(){dataLayer.push(arguments);}
+                gtag('js', new Date());
+
+                gtag('config', 'G-5XQNVP34CP');`}
+            </Script>
             <Component {...pageProps} />
         </main>
     )
